dc76a437a83162c97e71de16c99d9bd4c9c1fe71
santiago alejandro <santi20alejo@hotmail.com>
2020-06-03 14:59:13 -0500

    api-mascotas-php

---
+++ index.php
@@ -0,0 +1,38 @@
+<?php 
+$host ="23.236.50.121";
+$puerto = "3306";
+$user ="root";
+$pw = "matias2014se98";
+$db ="movil";
+function Conectarse($host,$puerto,$user,$pw,$db) {
+	if (!($link = mysqli_connect($host.":".$puerto, $user, $pw))){ 
+			echo "Error conectando a la base de datos.<br>"; 
+		   exit(); 
+		 }else{
+		  echo " ";
+		  }
+		  if (!mysqli_select_db($link, $db)) 
+		{ 
+			echo "Error seleccionando la base de datos.<br>"; 
+			exit(); 
+		}else{
+		   echo " "; 
+		 }
+	   return $link; 
+}
+//conectarse a la bd
+$link = Conectarse($host,$puerto,$user,$pw,$db);
+//metodo para ver los datos
+if ($_SERVER['REQUEST_METHOD'] == 'GET'){
+     $query="SELECT * FROM mascotas";
+ 	 $result = mysqli_query($link,$query);
+ 	 header("HTTP/1.1 200 OK");
+  	while($extraerDatos=$result->fetch_assoc()){
+        $resultado[]=$extraerDatos;
+    }
+        $json = json_encode($resultado);
+           echo $json;
+	
+}
+header("HTTP/1.1 400 Bad Request");
+ ?>
